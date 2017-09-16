1a8cd7cc0f9d5353c70609bde0d21f014d028b62
Oluseyi <anasey@outlook.com>
2017-09-16 18:42:04 -0400

    08 Truncate a string

---
+++ 08_Truncate_a_string.js
@@ -0,0 +1,31 @@
+/*
+Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+Return the truncated string with a ... ending.
+
+Note that inserting the three dots to the end will add to the string length.
+
+However, if the given maximum string length num is less than or equal to 3, 
+then the addition of the three dots does not add to the string length in determining the truncated string.
+
+Useful Method
+
+String.Prototype.slice();
+*/
+
+const truncateString = (str, num) => {
+    // console.log(num);
+    if (num <= 3){
+        return console.log(str.slice(0 , num).concat('...'));
+    }
+    if(num >= str.length){
+        return console.log(str);
+    }
+    return console.log(str.slice(0 , num-3).concat('...'));
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);//should return "A-tisket...".
+truncateString("A-", 1) //should return "A...".
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2) 
+//should return "A-tisket a-tasket A green and yellow basket".
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length)
+// should return "A-tisket a-tasket A green and yellow basket".
